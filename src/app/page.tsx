9404afadb6ea24d4ccfe7cfd32987739a1740416
Dango <jason543211@hotmail.com>
2023-10-12 03:28:00 -0700

    navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useRef } from 'react'
+import Image from 'next/image'
 import TestSection from '@/components/TestSection'
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
   const section1Ref = useRef<HTMLDivElement>(null)
   const section2Ref = useRef<HTMLDivElement>(null)
   const section3Ref = useRef<HTMLDivElement>(null)
+  const section4Ref = useRef<HTMLDivElement>(null)
 
   const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
     if (ref.current) {
@@ -21,10 +23,12 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       {/* Top section */}
-      <div className="top-10 left-10 flex space-x-4">
+      <div className="flex justify-center items-center w-screen h-60 gap-10">
         <button onClick={() => scrollToSection(section1Ref)}>Icon 1</button>
         <button onClick={() => scrollToSection(section2Ref)}>Icon 2</button>
+        <Image alt="logo" src="/artgumi-tiger.png" width={100} height={100} />
         <button onClick={() => scrollToSection(section3Ref)}>Icon 3</button>
+        <button onClick={() => scrollToSection(section4Ref)}>Icon 4</button>
       </div>
 
       {/* section 1 */}
@@ -33,14 +37,16 @@ export default function Home() {
       </div>
 
       {/* section 2 */}
-      {/* <div ref={section2Ref} className='w-screen h-[800px] bg-slate-500'>
-        222
-      </div> */}
       <TestSection ref={section2Ref} />
 
       {/* section 3 */}
       <div ref={section3Ref} className="w-screen h-[600px] bg-slate-900">
         333
+      </div>
+
+      {/* section 4 */}
+      <div ref={section4Ref} className="w-screen h-[900px] bg-slate-200">
+        444
       </div>
     </div>
   )
