8a4a0dcb9b1c9707df14490d954dab0e3232c6e1
Dango <jason543211@hotmail.com>
2023-11-08 19:30:49 -0800

    chore: refactor intro

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -12,50 +12,75 @@ const Intro = React.forwardRef<HTMLDivElement, IntroProps>(({ sections }, ref) =
     <div ref={ref} className="relative flex justify-center items-center w-screen h-screen">
       <IntroBackground />
 
-      <button className="absolute mb-[250px] mr-[500px] z-20" onClick={() => scrollToSection(sections[0])}>
-        <Image alt="note" src="/assets/intro/note-icon.png" width={350} height={350} className="h-auto w-auto" />
-      </button>
-
-      <button className="absolute mb-[300px] ml-[550px] z-20" onClick={() => scrollToSection(sections[1])}>
-        <Image alt="special" src="/assets/intro/special-icon.png" width={250} height={250} className="h-auto w-auto" />
-      </button>
-
-      {/* title */}
-      <div className="relative p-2 flex justify-center items-center">
-        <Image
-          alt="logo"
-          src="/assets/intro/title.png"
-          layout="responsive"
-          width={500}
-          height={500}
-          priority
-          className="w-auto h-auto"
-        />
-        <div className="absolute right-0 bottom-0 mb-2 sm:mb-3 md:mb-4 mr-4">
+      {/* intro content */}
+      <div className="relative flex justify-center items-center bg-blue-200 p-[10vw]">
+        <button className="absolute z-10 top-0 left-0" onClick={() => scrollToSection(sections[0])}>
           <Image
-            alt="label"
-            src="/assets/intro/label.png"
-            layout="responsive"
-            width={270}
-            height={270}
-            className="w-full h-auto"
+            alt="note"
+            src="/assets/intro/note-icon.png"
+            width={200}
+            height={200}
+            className="h-auto w-auto"
+            sizes="100vw"
           />
+        </button>
+
+        <button className="absolute z-10 top-0 right-0" onClick={() => scrollToSection(sections[1])}>
+          <Image
+            alt="special"
+            src="/assets/intro/special-icon.png"
+            width={200}
+            height={200}
+            className="h-auto w-auto"
+            sizes="100vw"
+          />
+        </button>
+
+        {/* title */}
+        <div className="relative p-2 flex justify-center items-center bg-red-300">
+          <Image
+            alt="logo"
+            src="/assets/intro/title.png"
+            width={500}
+            height={500}
+            priority
+            sizes="100vw"
+            className="w-auto h-auto"
+          />
+          <div className="absolute right-0 bottom-0 mb-2 sm:mb-3 md:mb-4 mr-4">
+            <Image
+              alt="label"
+              src="/assets/intro/label.png"
+              width={270}
+              height={270}
+              sizes="100vw"
+              className="w-auto h-auto"
+            />
+          </div>
         </div>
+
+        <button className="absolute bottom-0 left-0 z-10" onClick={() => scrollToSection(sections[2])}>
+          <Image
+            alt="highlight"
+            src="/assets/intro/highlight-icon.png"
+            width={200}
+            height={200}
+            className="h-auto w-auto"
+            sizes="100vw"
+          />
+        </button>
+
+        <button className="absolute bottom-0 right-0 z-10" onClick={() => scrollToSection(sections[3])}>
+          <Image
+            alt="credit"
+            src="/assets/intro/credit-icon.png"
+            width={200}
+            height={200}
+            className="h-auto w-auto"
+            sizes="100vw"
+          />
+        </button>
       </div>
-
-      <button className="absolute mt-[400px] mr-[570px] z-20" onClick={() => scrollToSection(sections[2])}>
-        <Image
-          alt="highlight"
-          src="/assets/intro/highlight-icon.png"
-          width={200}
-          height={200}
-          className="h-auto w-auto"
-        />
-      </button>
-
-      <button className="absolute mt-[250px] ml-[530px] z-20" onClick={() => scrollToSection(sections[3])}>
-        <Image alt="credit" src="/assets/intro/credit-icon.png" width={220} height={220} className="h-auto w-auto" />
-      </button>
     </div>
   )
 })
